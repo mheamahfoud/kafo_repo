116a85feeaa56e92b095158193705aa3764ffdaa
WINDOWS 10 <WINDOWS 10@DESKTOP-5E1P39L>
2022-10-26 07:31:16 +0300

    sync

--- app2/Repositories/Interfaces/SuccessStoryRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Interfaces;
-
-interface SuccessStoryRepositoryInterface extends EloquentRepositoryInterface {
-
-
- 
-}
--- app2/Repositories/Interfaces/SystemRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Interfaces;
-
-interface SystemRepositoryInterface extends EloquentRepositoryInterface {
-
-
- 
-}
